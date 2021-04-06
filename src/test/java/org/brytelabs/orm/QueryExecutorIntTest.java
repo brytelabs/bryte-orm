01610a424ed724a268e5d9e4fb4bc0662ff944c9
bbofosu <bbofosu@gmail.com>
2021-04-06 18:36:47 +0300

    fix test

--- src/test/java/org/brytelabs/orm/QueryExecutorIntTest.java
+++ src/test/java/org/brytelabs/orm/QueryExecutorIntTest.java
@@ -2,7 +2,6 @@ package org.brytelabs.orm;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
